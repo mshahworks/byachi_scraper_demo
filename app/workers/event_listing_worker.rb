5ad241131ffe9e51d3b57d6f68954b5d41af6d04
manan <manan@manan.cs>
2014-06-08 20:01:54 +0530

    some changes

--- app/workers/event_listing_worker.rb
+++ app/workers/event_listing_worker.rb
@@ -8,13 +8,16 @@ class EventListingWorker
     events_part = doc.search('.Content-Pane div[@class^=evsum]')
 
     events_part.each do |part|
-      type_name = get_type_name(part.attr('class'))
-      content    = part.search('.evdtl').last
-      start_date = nil
-      end_date   = nil
-      location   = nil
-      name       = nil
-      event_url  = nil
+      type_name   = get_type_name(part.attr('class'))
+      content     = part.search('.evdtl').last
+      start_date  = nil
+      end_date    = nil
+      location    = nil
+      name        = nil
+      event_url   = nil
+      org_name    = nil
+      org_email   = nil
+      description = nil
       if content.present?
         dates      = content.children.detect{|c| c.name == 'b'}.try(:text)
         dates_arr  = dates.split(' ')
@@ -32,15 +35,24 @@ class EventListingWorker
         name      = header.text
         event_url = header.children.attr('href').try(:value)
       end  
-      #if event_url.include?("baychi.org")
-      #  res = agent.get("http://www.baychi.org/program")
-      #  para = res.search('.Summary-Pane p')
-      #  loc = para.detect{|p| p.children.children.text == 'Location'}
-      #  org = para.detect{|p| p.children.children.text == 'BayCHI Contact'}
-      #  puts "==#{loc}===#{org}"
-      #  description = res.search('.Content-Pane').last.text
-      #end
-      Event.create(event_name: name, event_type: type_name, event_start_date: start_date, event_end_date: end_date, event_location: location, event_url: event_url)
+      if event_url.include?("baychi.org")
+        res = agent.get("http://www.baychi.org/program")
+        para = res.search('.Summary-Pane p')
+        loc = para.detect{|p| p.children.children.text == 'Location' || p.children.children.text == 'LocationDirections'}.try(:text)
+        loc = loc.gsub(/\n/,'').gsub(/Location/, '').gsub(/Directions/, '') if loc.present?
+        org = para.detect{|p| p.children.children.text.include?('BayCHI Contact')}.try(:text)
+        if org.present?
+          org_name  = org.split(/\n/)[1]
+          org_email = org.split(/\n/)[2]
+        end  
+        description = res.search('.Content-Pane').to_html
+      end
+      event = Event.where("event_name = ? and event_start_date = ? and event_end_date = ?", name, start_date, end_date).first
+      if event.present?
+        event.update_attributes(event_name: name, event_type: type_name, event_start_date: start_date, event_end_date: end_date, event_location: location, event_url: event_url, event_description: description, organizer_name: org_name, organizer_email: org_email)        
+      else
+        Event.create(event_name: name, event_type: type_name, event_start_date: start_date, event_end_date: end_date, event_location: location, event_url: event_url, event_description: description, organizer_name: org_name, organizer_email: org_email)
+      end
     end
 
     ## Past Events ##
